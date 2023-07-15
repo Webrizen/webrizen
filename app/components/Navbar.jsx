b6f781b905c4ea011c2d4d55080f150b14a3ffe0
WebRizen <webrizen@gmail.com>
2023-07-15 16:02:07 +0530

    kuch v

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,9 +1,72 @@
-import React from 'react'
+'use client';
+import React, { useState } from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
 
 export default function Navbar() {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const toggleDrawer = () => {
+    setIsDrawerOpen(!isDrawerOpen);
+  };
+
+  const menuItems = [
+    { label: 'Home', link: '/' },
+    { label: 'Services', link: '/services' },
+    { label: 'Portfolio', link: '/portfolio' },
+    { label: 'Products', link: '/products' },
+    { label: 'Get a proposal', link: '/proposal' },
+    { label: 'About', link: '/about' },
+    { label: 'Contact', link: '/contact' },
+    { label: 'Support', link: '/support' },
+    { label: 'Blogs', link: '/blogs' },
+    { label: 'Testimonials', link: '/testimonials' },
+    { label: 'Legal', link: '/legal' },
+
+  ];
+
   return (
-    <div>
-      Navbar
-    </div>
-  )
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static" style={{ background: 'linear-gradient(50deg, #5e17eb, #5271ff)' }}>
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+              onClick={toggleDrawer}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              WebRizen
+            </Typography>
+            <Button color="inherit">Login</Button>
+            <Button color="inherit">Signup</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+
+      <Drawer anchor="left" open={isDrawerOpen} onClose={toggleDrawer} >
+        <List sx={{ width: '250px' }}>
+          {menuItems.map((item, index) => (
+            <ListItem button key={index} component="a" href={item.link} sx={{ padding: '10px 20px' }}>
+              <ListItemText primary={item.label} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </>
+  );
 }
--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,9 @@
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import './globals.css';
-import { Inter } from 'next/font/google';
+import { Poppins } from 'next/font/google';
 
-const inter = Inter({ 
+const poppins = Poppins({ 
   subsets: ['latin'],
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
 })
@@ -16,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={poppins.className}>
         <Navbar/>
         {children}
         <Footer/>
