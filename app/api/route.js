7ac9d125ba5334718958fa695d7d52d69a250d3d
WebRizen <webrizen@gmail.com>
2023-07-18 17:44:24 +0530

    good

--- app/api/route.js
+++ app/api/route.js
@@ -16,14 +16,18 @@ export async function GET(request) {
       ],
       products: [
         {
+          id: "01",
           title: "SoundYank",
-          description: "The ultimate YouTube to MP3 converter and live streaming solution. With SoundYank, you can easily convert your favorite YouTube videos into high-quality MP3 format, allowing you to enjoy your favorite music, podcasts, interviews, and more on the go. Whether you want to stream your converted MP3 files directly on the web or download them for offline listening, SoundYank has got you covered.",
+          description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab, est doloribus voluptatem itaque quia nemo eius consectetur saepe deserunt sequi consequatur corporis! Quos et non vitae. Odio dicta ab molestiae?",
           link: "/products/soundyank.html",
+          image: "/placeholder.svg"
         },
         {
+          id: "02",
           title: "CodeNavy",
-          description: "CodeNavy is a powerful online code editor that allows developers to effortlessly paste, edit, and preview their code in real-time, all without the need for logging in. With CodeNavy, you can seamlessly write and experiment with HTML, CSS, and JavaScript code, testing your ideas instantly. Boost your productivity with our intuitive interface and convenient features, empowering you to collaborate, share, and iterate on your code hassle-free. Experience the freedom to code anytime, anywhere with CodeNavy, the ultimate solution for seamless online code editing and previewing.",
+          description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab, est doloribus voluptatem itaque quia nemo eius consectetur saepe deserunt sequi consequatur corporis! Quos et non vitae. Odio dicta ab molestiae?",
           link: "/products/codenavy.html",
+          image: "/placeholder.svg"
         },
       ],
       portfolio: [
@@ -42,6 +46,16 @@ export async function GET(request) {
           image: "/placeholder.svg",
         }
       ],
+      about: [
+        {
+          image: "/copy.svg",
+          text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab, est doloribus voluptatem itaque quia nemo eius consectetur saepe deserunt sequi consequatur corporis! Quos et non vitae. Odio dicta ab molestiae?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab, est doloribus voluptatem itaque quia nemo eius consectetur saepe deserunt sequi consequatur corporis! Quos et non vitae. Odio dicta ab molestiae?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab, est doloribus voluptatem itaque quia nemo eius consectetur saepe deserunt sequi consequatur corporis! Quos et non vitae. Odio dicta ab molestiae?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab, est doloribus voluptatem itaque quia nemo eius consectetur saepe deserunt sequi consequatur corporis! Quos et non vitae. Odio dicta ab molestiae?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab, est doloribus voluptatem itaque quia nemo eius consectetur saepe deserunt sequi consequatur corporis! Quos et non vitae. Odio dicta ab molestiae?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab, est doloribus voluptatem itaque quia nemo eius consectetur saepe deserunt sequi consequatur corporis! Quos et non vitae. Odio dicta ab molestiae?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab, est doloribus voluptatem itaque quia nemo eius consectetur saepe deserunt sequi consequatur corporis! Quos et non vitae. Odio dicta ab molestiae?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab, est doloribus voluptatem itaque quia nemo eius consectetur saepe deserunt sequi consequatur corporis! Quos et non vitae. Odio dicta ab molestiae?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab, est doloribus voluptatem itaque quia nemo eius consectetur saepe deserunt sequi consequatur corporis! Quos et non vitae. Odio dicta ab molestiae?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab, est doloribus voluptatem itaque quia nemo eius consectetur saepe deserunt sequi consequatur corporis! Quos et non vitae. Odio dicta ab molestiae?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab, est doloribus voluptatem itaque quia nemo eius consectetur saepe deserunt sequi consequatur corporis! Quos et non vitae. Odio dicta ab molestiae?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab, est doloribus voluptatem itaque quia nemo eius consectetur saepe deserunt sequi consequatur corporis! Quos et non vitae. Odio dicta ab molestiae?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab, est doloribus voluptatem itaque quia nemo eius consectetur saepe deserunt sequi consequatur corporis! Quos et non vitae. Odio dicta ab molestiae?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab, est doloribus voluptatem itaque quia nemo eius consectetur saepe deserunt sequi consequatur corporis! Quos et non vitae. Odio dicta ab molestiae?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab, est doloribus voluptatem itaque quia nemo eius consectetur saepe deserunt sequi consequatur corporis! Quos et non vitae. Odio dicta ab molestiae?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab, est doloribus voluptatem itaque quia nemo eius consectetur saepe deserunt sequi consequatur corporis! Quos et non vitae.",
+          phone: 918761820422,
+          email: "webrizen@gmail.com",
+          instagram: "https://instagram.com/Arshahdul_ahmed",
+          whatsapp: "https://wa.me/+918761820422"
+        }
+      ]
     };
   
     return new Response(JSON.stringify(data), {
