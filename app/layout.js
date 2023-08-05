8122909a29e6033ed03ca3b1fe5865449f78ed75
WebRizen <webrizen@gmail.com>
2023-08-05 18:33:25 +0530

    update

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,4 @@
 import Footer from './components/Footer';
-import GlobalBreadcrumbs from './components/GlobalBreadcrumbs';
 import Navbar from './components/Navbar';
 import './globals.css';
 
@@ -13,7 +12,6 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
         <Navbar/>
-        <GlobalBreadcrumbs/>
         {children}
         <Footer/>
         </body>
