287eee2cc4b680dabbe6800715ce521e33337eeb
WebRizen <webrizen@gmail.com>
2024-05-11 19:50:22 +0530

    Added privacy-policy and terms-of-service pages

---
+++ src/app/privacy-policy/page.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <>
+      <section className="py-16">
+        <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="px-8 py-10 lg:py-12">
+            <h2 className="text-3xl lg:text-4xl font-semibold text-gray-800 dark:text-white mb-6">
+              Privacy Policy
+            </h2>
+            <p className="text-gray-700 dark:text-gray-300 mb-6">
+              At Webrizen, accessible from www.webrizen.com, one of our main
+              priorities is the privacy of our visitors. This Privacy Policy
+              document contains types of information that is collected and
+              recorded by Webrizen and how we use it.
+            </p>
+            <p className="text-gray-700 dark:text-gray-300 mb-6">
+              If you have additional questions or require more information about
+              our Privacy Policy, do not hesitate to contact us.
+            </p>
+            <p className="text-gray-700 dark:text-gray-300 mb-6">
+              This Privacy Policy applies only to our online activities and is
+              valid for visitors to our website with regards to the information
+              that they shared and/or collect in Webrizen. This policy is not
+              applicable to any information collected offline or via channels
+              other than this website.
+            </p>
+            <h3 className="text-xl lg:text-2xl font-semibold text-gray-800 dark:text-white mb-4">
+              Consent
+            </h3>
+            <p className="text-gray-700 dark:text-gray-300 mb-6">
+              By using our website, you hereby consent to our Privacy Policy and
+              agree to its terms.
+            </p>
+            <h3 className="text-xl lg:text-2xl font-semibold text-gray-800 dark:text-white mb-4">
+              Information we collect
+            </h3>
+            <p className="text-gray-700 dark:text-gray-300 mb-6">
+              The personal information that you are asked to provide, and the
+              reasons why you are asked to provide it, will be made clear to you
+              at the point we ask you to provide your personal information.
+            </p>
+            <p className="text-gray-700 dark:text-gray-300 mb-6">
+              If you contact us directly, we may receive additional information
+              about you such as your name, email address, phone number, the
+              contents of the message and/or attachments you may send us, and
+              any other information you may choose to provide.
+            </p>
+            {/* Add more sections as needed */}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
