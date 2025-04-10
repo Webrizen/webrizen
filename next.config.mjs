feb076f419919821232b79b2acb00b4fd1d2dd34
WebRizen <webrizen@gmail.com>
2025-04-10 13:54:15 +0530

    updated - nextjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        domains: ['images.unsplash.com', 'cdn.sanity.io']
-    }
-};
+const nextConfig = {};
 
 export default nextConfig;
