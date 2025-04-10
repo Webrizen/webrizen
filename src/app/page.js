2713755930cecc5eb9137b1f2a45d7797c2cd5cd
WebRizen <webrizen@gmail.com>
2025-04-10 14:54:41 +0530

    feat: add ThemeProvider component for managing theme context

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
-import { Button } from "@/components/ui/button"
+import React from 'react'
 
 export default function Home() {
   return (
     <div>
-      <Button>Click me</Button>
+      it's a fucking home page!
     </div>
   )
 }
